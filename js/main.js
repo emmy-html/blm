4bf5572e529f587980092354d532cefb77990d5e
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-10 18:48:33 -0500

    Added a light mode/dark mode toggle option

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// fades in content on load
 $(document).ready(function () {
     $('.fade').fadeIn(1500).removeClass('fade');
 });
+// toggle color scheme switch
+function toggle() {
+    var element = document.body;
+    var modeText = document.getElementById("toggle-description");
+    var modeToggle = document.getElementById("mode-toggle").checked;
+    element.classList.toggle("blackout-mode");
+    if (modeToggle === false) {
+    modeText.innerHTML = "Blackout Mode";
+    } else {
+        modeText.innerHTML = "Light Mode";
+    }
+}

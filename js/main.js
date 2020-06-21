63d39efe4cf3b6f519dcd9d03a7cbb5d298e770d
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-21 14:48:52 -0500

    Added functionality to a filter feature that allows different resources to be displayed based on the category

--- js/main.js
+++ js/main.js
@@ -35,5 +35,49 @@ function openResource(evt, topic) {
     document.getElementById(topic).style.display = "block";
     evt.currentTarget.className += " active";
 }
-// show a tab by default
+// show a resource tab by default
 document.getElementById("default").click();
+// filter through resources
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c === "all") c = "";
+    // Adds the "show" class to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+}
+// show filtered elements
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+// hide elements that are not selected
+function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+// add active class to the current control button
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active-filter");
+        current[0].className = current[0].className.replace(" active-filter", "");
+        this.className += " active-filter";
+    });
+}

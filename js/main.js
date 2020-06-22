d61422314812a7425f0fddab2471cc173c66a86a
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-22 18:26:40 -0500

    Changed code for color scheme switcher that remembers the users' choice

--- js/main.js
+++ js/main.js
@@ -2,24 +2,72 @@
 $(document).ready(function () {
     $('.fade').fadeIn(1000).removeClass('fade');
 });
+
+// show a category tab by default
+document.getElementById("default").click();
 // toggle color scheme switch
 function toggle() {
-    var element = document.body;
     var modeText = document.getElementById("toggle-description");
-    var modeToggle = document.getElementById("mode-toggle").checked;
-    element.classList.toggle("blackout-mode");
-    if (modeToggle === false) {
-    modeText.innerHTML = "Blackout Mode";
+    var modeToggle = document.getElementById("themeSwitch").checked;
+    if (modeToggle) {
+        modeText.innerHTML = "Blackout Mode";
     } else {
         modeText.innerHTML = "Light Mode";
     }
+}
+// NEW color theme switch
+document.getElementById('themeSwitch').addEventListener('change', function(event){
+    (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+  });
+var themeSwitch = document.getElementById('themeSwitch');
+if(themeSwitch) {
+  themeSwitch.addEventListener('change', function(event){
+    resetTheme(); // update color theme
+  });
+
+  function resetTheme() {
+    if(themeSwitch.checked) { // dark theme has been selected
+      document.body.setAttribute('data-theme', 'dark');
+      localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+    } else {
+      document.body.removeAttribute('data-theme');
+      localStorage.removeItem('themeSwitch'); // reset theme selection 
+    } 
+  };
+}
+var themeSwitch = document.getElementById('themeSwitch');
+if(themeSwitch) {
+  initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+  themeSwitch.addEventListener('change', function(event){
+    resetTheme(); // update color theme
+  });
+
+  function initTheme() {
+    var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark');
+    // update checkbox
+    themeSwitch.checked = darkThemeSelected;
+    // update body data-theme attribute
+    darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.setAttribute('data-theme', 'light');
+  };
+
+  function resetTheme() {
+    if(themeSwitch.checked) { // dark theme has been selected
+      document.body.setAttribute('data-theme', 'dark');
+      localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+      localStorage.removeItem('themeSwitch'); // reset theme selection 
+    } 
+  };
 }
 // toggle the about section
 $(".about").click(function() {
     $("#about").fadeToggle('linear');
 })
-// show a resource tab by default
-document.getElementById("default").click();
+// date updated function
+var dateDisplay = document.getElementById("date");
+dateDisplay.innerHTML = "June 22nd, 2020";
 // filter through resources
 filterSelection("all")
 function filterSelection(c) {

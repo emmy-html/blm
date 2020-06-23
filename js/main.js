317d344891fa915a6e6726ee1674aca5349c8df8
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-23 11:19:35 -0500

    Split JS files to improve performance for all features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // fades in content on load
 $(document).ready(function () {
-    $('.fade').fadeIn(1000).removeClass('fade');
+    $('.fade').fadeIn(1500).removeClass('fade');
 });
 // NEW color theme switch
 document.getElementById('themeSwitch').addEventListener('change', function(event){
@@ -56,57 +56,8 @@ if(themeSwitch) {
 $(".about").click(function() {
     $("#about").fadeToggle('linear');
 })
-
 // show a category tab by default
 document.getElementById("default").click();
 // date updated function
 var dateDisplay = document.getElementById("date");
 dateDisplay.innerHTML = "June 22nd, 2020";
-// filter through resources
-filterSelection("all")
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterLink");
-    if (c === "all") c = "";
-    // Adds the "show" class to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-        removeClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
-    }
-}
-// show filtered elements
-function addClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
-        }
-    }
-}
-// hide elements that are not selected
-function removeClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
-}
-// add active class to the current control button
-var btns = document.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active-filter");
-        current[0].className = current[0].className.replace(" active-filter", "");
-        this.className += " active-filter";
-    });
-}
-// testing nesting the function
-function findTag(c) {
-    filterSelection(c);
-}
